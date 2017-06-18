7e162d80f61fa81b9b4c214e961e28ce3ec8436d
Zehui <zy18763@nottingham.edu.cn>
2017-06-18 16:04:00 +0800

    add the basic framework of QnA_bot

---
+++ basic_frame_work/Bot Application1/Bot Application1/Dialogs/RootDialog.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Bot_Application1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // calculate something for us to return
+           // int length = (activity.Text ?? string.Empty).Length;
+            var question = activity.Text;
+           // var response = $"问题\"{question}\"的答案是：答案";
+
+            // return our reply to the user
+           
+
+
+
+            string responseString = string.Empty;
+
+            var query = question; //User Query
+            var knowledgebaseId = "a80ffe18-c56f-487e-91d4-d6ee495223ab"; // Use knowledge base id created.
+            var qnamakerSubscriptionKey = "904d93ec99d440969c82d25ddaebf814"; //Use subscription key assigned to you.
+
+            //Build the URI
+            Uri qnamakerUriBase = new Uri("https://westus.api.cognitive.microsoft.com/qnamaker/v1.0");
+            var builder = new UriBuilder($"{qnamakerUriBase}/knowledgebases/{knowledgebaseId}/generateAnswer");
+
+            //Add the question as part of the body
+            var postBody = $"{{\"question\": \"{query}\"}}";
+
+            //Send the POST request
+            using (WebClient client = new WebClient())
+            {
+                //Set the encoding to UTF8
+                client.Encoding = System.Text.Encoding.UTF8;
+
+                //Add the subscription key header
+                client.Headers.Add("Ocp-Apim-Subscription-Key", qnamakerSubscriptionKey);
+                client.Headers.Add("Content-Type", "application/json");
+                responseString = client.UploadString(builder.Uri, postBody);
+                QnAMakerResult response;
+                try
+                {
+                    response = JsonConvert.DeserializeObject<QnAMakerResult>(responseString);
+                }
+                catch
+                {
+                    throw new Exception("Unable to deserialize QnA Maker response string.");
+                }
+                await context.PostAsync(response.Answer);
+            }
+
+
+
+
+
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+
+
+    public class QnAMakerResult
+    {
+        /// <summary>
+        /// The top answer found in the QnA Service.
+        /// </summary>
+        [JsonProperty(PropertyName = "answer")]
+        public string Answer { get; set; }
+
+        /// <summary>
+        /// The score in range [0, 100] corresponding to the top answer found in the QnA    Service.
+        /// </summary>
+        [JsonProperty(PropertyName = "score")]
+        public double Score { get; set; }
+    }
+
+
+}
---
+++ basic_frame_work/Bot Application1/Bot Application1/Dialogs/SimpleLuisDialog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace Bot_Application1.Dialogs
+{
+    [LuisModel("f6b501c3-9ad2-4ce6-a086-3d9f2c0c329b","5ae56471cf00425f8f949ead61af47fd" )]
+    [Serializable]
+    public class SimpleLuisDialog : LuisDialog<object>
+    {
+        public const string Department = "院系";
+        public const string Position = "职位";
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("None")]
+        private async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"对不起，我不太理解你的请求：";
+            await context.PostAsync(message);
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("查询人名")]
+        private async Task findName(IDialogContext context, LuisResult result)
+        {
+            EntityRecommendation department;
+            if (!result.TryFindEntity(Department, out department))
+            {
+                await context.PostAsync("请明确查询人的院系");
+            }
+            else
+            {
+                await context.PostAsync($"输入人的输入的院系是{department.Entity}");
+            }
+            context.Wait(MessageReceived);
+        }
+
+
+
+    }
+}
